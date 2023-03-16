cca0a652cf8af538c989c3c95f30b0e6de015fa7
Moroz Pavlo <pavlo@sidekickinteractive.com>
2023-03-16 08:49:30 -0400

    Initial Commit

---
+++ FirebaseApp/FirebaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAppApp.swift
+//  FirebaseApp
+//
+//  Created by Moroz Pavlo on 2023-03-16.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
